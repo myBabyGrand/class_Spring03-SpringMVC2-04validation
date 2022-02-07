9d125f42364ffa5bc9ac8c1e73d9e3f941286fe4
mybabygrand <amuro_ray@kakao.com>
2022-02-07 22:22:43 +0900

    Section5. Validation  - V1. directly realization.

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -5,10 +5,13 @@ import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/validation/v1/items")
@@ -38,7 +41,34 @@ public class ValidationItemControllerV1 {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+
+        //검증 오류 결과 관리
+        Map<String, String> errors = new HashMap<String, String>();
+        //검증로직 - 필수값체크
+        if(!StringUtils.hasText(item.getItemName())){
+            errors.put("itemName", "상품 이름은 필수입니다.");
+        }
+        if(item.getPrice()==null || item.getPrice()<1000 || item.getPrice()>1000000){
+            errors.put("price", "가격은 1,000 ~ 1,000,000까지 입력가능합니다.");
+        }
+        if(item.getQuantity()==null || item.getQuantity()>9999 ){
+            errors.put("quantity", "수량은 최대 9,999까지 입력가능합니다.");
+        }
+        //검증로직 - 비즈니스정의
+        if(item.getPrice() != null && item.getQuantity() != null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice<10000){
+                errors.put("globalError", "가격*수량 은 10,000원 이상이어야 합니다. 현재값 = "+resultPrice);
+            }
+        }
+
+        //에러가 있다면 다시 입력 폼으로
+        if(!errors.isEmpty()){
+            model.addAttribute("errors", errors);
+            return "validation/v1/addForm";
+        }
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
